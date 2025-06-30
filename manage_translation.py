6b097bc170977be17ae01673d60cb75c7d3d7656
Stan Ulbrych <stanulbrych@gmail.com>
2025-06-30 14:25:57 +0100

    Backport: Add Last-Translators to TX pull commit message (#88)

--- manage_translation.py
+++ manage_translation.py
@@ -11,6 +11,7 @@
 #          files.
 # * recreate_tx_config: recreate configuration for all resources.
 # * warn_about_files_to_delete: lists files that are not available upstream
+# * generate_commit_msg: generates commit message with co-authors
 
 from argparse import ArgumentParser
 import os
@@ -19,16 +20,18 @@ from dataclasses import dataclass
 from difflib import SequenceMatcher
 from logging import info
 from pathlib import Path
-from subprocess import call
+from subprocess import call, run, CalledProcessError
 import sys
 from tempfile import TemporaryDirectory
 from typing import Self, Generator, Iterable
 from warnings import warn
 
-from polib import pofile
+from polib import pofile, POFile
 from transifex.api import transifex_api
 
 LANGUAGE = 'pl'
+PROJECT_SLUG = 'python-newest'
+VERSION = '3.14'
 
 
 def fetch():
@@ -49,10 +52,6 @@ def _call(command: str):
         exit(return_code)
 
 
-PROJECT_SLUG = 'python-313'
-VERSION = '3.13'
-
-
 def recreate_tx_config():
     """
     Regenerate Transifex client config for all resources.
@@ -71,11 +70,13 @@ def recreate_tx_config():
             file.write(contents)
     warn_about_files_to_delete()
 
+
 def warn_about_files_to_delete():
     files = list(_get_files_to_delete())
     if not files:
         return
     warn(f'Found {len(files)} file(s) to delete: {", ".join(files)}.')
+
 
 def _get_files_to_delete():
     with open('.tx/config') as config_file:
@@ -86,11 +87,13 @@ def _get_files_to_delete():
 
 
 def _clone_cpython_repo(version: str):
-    _call(f'git clone -b {version} --single-branch https://github.com/python/cpython.git --depth 1')
+    _call(
+        f'git clone -b {version} --single-branch https://github.com/python/cpython.git --depth 1'
+    )
 
 
 def _build_gettext():
-    _call("make -C cpython/Doc/ gettext")
+    _call('make -C cpython/Doc/ gettext')
 
 
 def _create_txconfig():
@@ -115,7 +118,9 @@ class ResourceLanguageStatistics:
     @classmethod
     def from_api_entry(cls, data: transifex_api.ResourceLanguageStats) -> Self:
         return cls(
-            name=data.id.removeprefix(f'o:python-doc:p:{PROJECT_SLUG}:r:').removesuffix(f':l:{LANGUAGE}'),
+            name=data.id.removeprefix(f'o:python-doc:p:{PROJECT_SLUG}:r:').removesuffix(
+                f':l:{LANGUAGE}'
+            ),
             total_words=data.attributes['total_words'],
             translated_words=data.attributes['translated_words'],
             total_strings=data.attributes['total_strings'],
@@ -145,10 +150,17 @@ def get_resource_language_stats() -> list[ResourceLanguageStatistics]:
     return [ResourceLanguageStatistics.from_api_entry(entry) for entry in resources]
 
 
-def progress_from_resources(resources: Iterable[ResourceLanguageStatistics]) -> float:
-    pairs = ((e.translated_words, e.total_words) for e in resources)
-    translated_total, total_total = (sum(counts) for counts in zip(*pairs))
-    return translated_total / total_total * 100
+def progress_from_resources(
+    resources: Iterable[ResourceLanguageStatistics],
+) -> tuple[float, float]:
+    word_pairs = ((e.translated_words, e.total_words) for e in resources)
+    string_pairs = ((e.translated_strings, e.total_strings) for e in resources)
+    translated_total_words, total_words = (sum(counts) for counts in zip(*word_pairs))
+    translated_total_strs, total_strs = (sum(counts) for counts in zip(*string_pairs))
+    return (
+        translated_total_words / total_words * 100,
+        translated_total_strs / total_strs * 100,
+    )
 
 
 def get_number_of_translators():
@@ -161,21 +173,23 @@ def get_number_of_translators():
 def _fetch_translators() -> Generator[str, None, None]:
     for file in Path().rglob('*.po'):
         header = pofile(file).header.splitlines()
-        for translator_record in header[header.index('Translators:') + 1:]:
+        for translator_record in header[header.index('Translators:') + 1 :]:
             translator, _year = translator_record.split(', ')
             yield translator
 
 
 def _remove_bot(translators: set[str]) -> None:
-    translators.remove("Transifex Bot <>")
+    translators.remove('Transifex Bot <>')
 
 
 def _eliminate_aliases(translators: set[str]) -> set[str]:
     unique = set()
     for name in translators:
         for match in unique:
-            if (ratio := SequenceMatcher(lambda x: x in '<>@', name, match).ratio()) > 0.64:
-                info(f"{name} and {match} are similar ({ratio:.3f}). Deduplicating.")
+            if (
+                ratio := SequenceMatcher(lambda x: x in '<>@', name, match).ratio()
+            ) > 0.64:
+                info(f'{name} and {match} are similar ({ratio:.3f}). Deduplicating.')
                 break
         else:
             unique.add(name)
@@ -184,11 +198,67 @@ def _eliminate_aliases(translators: set[str]) -> set[str]:
 
 def language_switcher(entry: ResourceLanguageStatistics) -> bool:
     language_switcher_resources_prefixes = ('bugs', 'tutorial', 'library--functions')
-    return any(entry.name.startswith(prefix) for prefix in language_switcher_resources_prefixes)
+    return any(
+        entry.name.startswith(prefix) for prefix in language_switcher_resources_prefixes
+    )
 
 
-if __name__ == "__main__":
-    RUNNABLE_SCRIPTS = ('fetch', 'recreate_tx_config', 'warn_about_files_to_delete')
+def generate_commit_msg():
+    """Generate a commit message
+    Parses staged files and generates a commit message with Last-Translator's as
+    co-authors.
+    """
+    translators: set[str] = set()
+
+    result = run(
+        ['git', 'diff', '--cached', '--name-only', '--diff-filter=ACM'],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    staged = [
+        filename for filename in result.stdout.splitlines() if filename.endswith('.po')
+    ]
+
+    for file in staged:
+        staged_file = run(
+            ['git', 'show', f':{file}'], capture_output=True, text=True, check=True
+        ).stdout
+        try:
+            old_file = run(
+                ['git', 'show', f'HEAD:{file}'],
+                capture_output=True,
+                text=True,
+                check=True,
+            ).stdout
+        except CalledProcessError:
+            old_file = ''
+
+        new_po = pofile(staged_file)
+        old_po = pofile(old_file) if old_file else POFile()
+        old_entries = {entry.msgid: entry.msgstr for entry in old_po}
+
+        for entry in new_po:
+            if entry.msgstr and (
+                entry.msgid not in old_entries
+                or old_entries[entry.msgid] != entry.msgstr
+            ):
+                translator = new_po.metadata.get('Last-Translator')
+                translator = translator.split(',')[0].strip()
+                if translator:
+                    translators.add(f'Co-Authored-By: {translator}')
+                break
+
+    print('Update translation from Transifex\n\n' + '\n'.join(translators))
+
+
+if __name__ == '__main__':
+    RUNNABLE_SCRIPTS = (
+        'fetch',
+        'recreate_tx_config',
+        'warn_about_files_to_delete',
+        'generate_commit_msg',
+    )
 
     parser = ArgumentParser()
     parser.add_argument('cmd', choices=RUNNABLE_SCRIPTS)
